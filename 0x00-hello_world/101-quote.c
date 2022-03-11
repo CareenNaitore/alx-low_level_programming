8bf4b6624c93b1c1fcc123b96d48849fccabd99f
careen naitore <careennaitore@gmail.com>
2022-03-11 00:38:07 -0800

    prints quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,26 @@
+/*
+ *
+ * file quote 100
+ *
+ *
+ *
+ */
+
+#include <stdio.h>
+
+/*
+ *
+ *
+ *files generates quotes
+ *created by careen naitore
+ *
+ *
+ */
+int main (void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+		return (1);
+}
