95c509e2b415bc26e07480d2323f0e1e72b2cf8a
careen naitore <careennaitore@gmail.com>
2022-03-15 01:56:29 -0700

    function nested directory

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/*
+ *
+ *
+ * question:Write a program that prints _putchar, followed by a new line
+ * author careen naitore
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+
+		return (0);
+}
+	
