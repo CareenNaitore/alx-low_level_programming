db5955b694ea18f55aa9072c161470310c7bf14e
CareenNaitore <42148220+CareenNaitore@users.noreply.github.com>
2022-03-17 13:50:23 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,10 @@
 #include "main.h"
-/*
- *
- *
- *
- * file 0 is used to check if c is capial
+
+/**
+ * _isupper - Checks for uppercase characters.
  * @c: The character to be checked.
  *
  * Return: 1 if character is uppercase, 0 otherwise.
- *
  */
 int _isupper(int c)
 {
