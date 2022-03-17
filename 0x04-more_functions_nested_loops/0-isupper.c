e2624dc8bb602cb6ce5e028837d665b945546ec0
careen naitore <careennaitore@gmail.com>
2022-03-17 03:39:45 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *
+ *
+ *
+ * file 0 is used to check if c is capial
+ * @c: The character to be checked.
+ *
+ *
+ *
+ */
+int _isupper(int c)                                                                                                                                                                    
+{                                                                                                                                                                                      
+	        if (c >= 'a' && c <= 'z')
+			return (1);                                                                                                                                                            
+	        else
+			return (0);                                                                                                                                                            
+}                                                                                                                                                                                      
