7b0f6a7e1215fe3dc81be4550512b1c70244d761
CareenNaitore <42148220+CareenNaitore@users.noreply.github.com>
2022-03-21 18:48:26 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,11 @@
  *
  * Return: The length of @str.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
 	size_t length = 0;
 
-	while (*str++)
+	while (*s++)
 		length++;
 
 	return (length);
