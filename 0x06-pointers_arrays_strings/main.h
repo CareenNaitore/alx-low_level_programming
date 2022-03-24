8ab04e49b8fdf9676ba6194b246f22f02e4f3a81
CareenNaitore <42148220+CareenNaitore@users.noreply.github.com>
2022-03-24 15:20:18 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,7 +19,7 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void rev_string(char *n)
+void rev_string(char *n);
 void print_buffer(char *b, int size);
 
 #endif
