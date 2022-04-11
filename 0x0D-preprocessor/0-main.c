db937b3798cc33d73b547c47e7524cb754da4414
careen <careennaitore@gmail.com>
2022-04-11 22:33:26 +0300

    edit

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,19 @@
+#include "../0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int s;
+
+s = 98 + SIZE;
+printf("%d\n", s);
+return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+P
