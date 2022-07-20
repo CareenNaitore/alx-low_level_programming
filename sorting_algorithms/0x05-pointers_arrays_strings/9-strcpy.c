984c9507a34b5efdbe4d48835f317c31283eec23
“[Careen <careennaitore@gmail.com>
2022-07-20 09:01:28 +0300

    Big o Notation

---
+++ sorting_algorithms/0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strcpy - copy strg to another string
+ * @dest: pointer
+ * @src: poiter
+ * Return: numbers dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = src[i];
+	return (dest);
+}
